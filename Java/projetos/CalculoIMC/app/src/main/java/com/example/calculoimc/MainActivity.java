b880395b190163b23e0bfb12d2e075d82773abd7
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2024-01-12 21:55:40 -0300

    projeto calculadora imc

---
+++ Java/projetos/CalculoIMC/app/src/main/java/com/example/calculoimc/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.calculoimc;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.calculoimc.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    //COMECA AQUI O CODIGO
+
+
+    }
+}
