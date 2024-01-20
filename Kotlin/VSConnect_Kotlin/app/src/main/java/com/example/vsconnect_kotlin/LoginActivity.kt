680968b4fbfc59d20e50e270dc2eaa7591261c1c
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2024-01-19 21:56:26 -0300

    finalizacao do login e inicio de servicos

--- Kotlin/VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ Kotlin/VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vsconnect_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
@@ -11,6 +12,22 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        //Atribui a variavel binding um objeto que contem referencias(propriedades) aos elementos definidos no layout
+
+        binding = ActivityLoginBinding.inflate(layoutInflater) //alteracao a partir desse arquivo vai salva
+
+        binding.btnEntrar.setOnClickListener {
+            //variavel mainIntent com a intencao de sair da LoginActivity e ir para MainActivity
+            val mainIntent = Intent(this@LoginActivity,
+                MainActivity::class.java) //val e uma CONST do JS
+
+            //Executa a intecao armazenada na variavel mainIntent
+            startActivity(mainIntent)
+
+            //finaliza a LoginActivity
+            finish()
+        }
+        setContentView(binding.root)//alteracao a partir desse arquivo vai salva
     }
 }
