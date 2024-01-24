b2d3124c2275f90d05512bfd373a80fc3c52ceb8
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2024-01-23 21:52:57 -0300

    Inicio da api

--- Kotlin/VSConnect_Kotlin/app/build.gradle.kts
+++ Kotlin/VSConnect_Kotlin/app/build.gradle.kts
@@ -36,6 +36,11 @@ android {
 }
 
 dependencies {
+    //retrofit
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     //dependencia que arredonda imagem - circleimageview
     implementation("de.hdodenhof:circleimageview:3.1.0")
