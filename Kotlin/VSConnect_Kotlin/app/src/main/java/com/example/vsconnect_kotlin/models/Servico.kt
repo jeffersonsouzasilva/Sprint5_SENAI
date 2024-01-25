a2bbab17f8ad2d895b875089ba3fa0e54014b411
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2024-01-24 21:52:50 -0300

    consumo de api e teste no insomnia

---
+++ Kotlin/VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/models/Servico.kt
@@ -0,0 +1,10 @@
+package com.example.vsconnect_kotlin
+
+import java.util.UUID
+
+class Servico (
+    val idServico: UUID,
+    val titulo: String,
+    val descricao: String,
+    val proposta: String
+)
